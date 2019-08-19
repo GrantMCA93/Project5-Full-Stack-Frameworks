7d23b47c4f2e648d99da33c768a21bf1d4c8a369
Ubuntu <ubuntu@ip-172-31-19-207.eu-west-1.compute.internal>
2019-08-19 12:25:56 +0000

    trying to fix stripe_id issue

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-print(SECRET_KEY)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -170,7 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', "pk_test_Pk5ONCJOfc6RMhGwhHGA1DeA00Nx2FihbI")
-STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET', "sk_test_vVYdI8zPFgTNQizj1Vl4AODx00j3btodN2")
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
